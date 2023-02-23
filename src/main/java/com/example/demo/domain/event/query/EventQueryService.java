310afddc5934b22a9a7f47069c93ecf741a1fbbe
mimasala <65661176+mimasala@users.noreply.github.com>
2023-02-23 10:43:42 +0100

    create event CRUD operations + swagger

--- src/main/java/com/example/demo/domain/event/query/EventQueryService.java
+++ src/main/java/com/example/demo/domain/event/query/EventQueryService.java
@@ -1,7 +1,26 @@
 package com.example.demo.domain.event.query;
 
+import com.example.demo.domain.event.Event;
+import com.example.demo.domain.event.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EventQueryService {
+    private final EventRepository eventRepository;
+    @Autowired
+    public EventQueryService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Event getEvent(UUID id) {
+        return eventRepository.findById(id).orElseThrow(); //TODO: handle not found
+    }
 }
