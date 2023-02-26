84673e10ced82fffbe2178a536c419c238e4bd60
noelhug <89466986+noelhug@users.noreply.github.com>
2023-02-26 10:00:49 +0100

    Add log for failed to retrieve labels

--- src/main/java/com/example/demo/domain/event/asyncEvents/EventCreationEventHandler.java
+++ src/main/java/com/example/demo/domain/event/asyncEvents/EventCreationEventHandler.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.event.asyncEvents;
 
 import com.example.demo.core.exception.OpenAIResponseUnprocessableException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+@Log4j2
 public class EventCreationEventHandler implements ApplicationListener<EventCreatedMessage> {
     private final TextAnalyzer analyzer;
 
@@ -25,6 +27,7 @@ public class EventCreationEventHandler implements ApplicationListener<EventCreat
         try {
             prediction = analyzer.getLabelsForText(event.getEvent().getEventName(), 5);
         } catch (OpenAIResponseUnprocessableException e) {
+            log.error("Failed to find label for event: " + event.getEvent().getId());
             return; //TODO(hugn): Find a better behavior if the previous step fails, like send to human reviewer.
         }
 
