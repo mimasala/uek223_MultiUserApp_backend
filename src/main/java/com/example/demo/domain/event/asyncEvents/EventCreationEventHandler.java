208523df6fbd4c66c6c7daf9d43f41b773b122fe
noelhug <89466986+noelhug@users.noreply.github.com>
2023-02-26 15:58:41 +0100

    Fuck GCP Secret manager, so annoying smh

--- src/main/java/com/example/demo/domain/event/asyncEvents/EventCreationEventHandler.java
+++ src/main/java/com/example/demo/domain/event/asyncEvents/EventCreationEventHandler.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -45,6 +46,8 @@ public class EventCreationEventHandler implements ApplicationListener<EventCreat
         } catch (OpenAIResponseUnprocessableException e) {
             log.error("Failed to find label for event: " + event.getEvent().getId());
             return; //TODO(hugn): Find a better behavior if the previous step fails, like send to human reviewer.
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
         String labelString = "[\"" + String.join("\", \"", labels) + "\"]";
--- src/main/java/com/example/demo/domain/event/asyncEvents/TextAnalyzer.java
+++ src/main/java/com/example/demo/domain/event/asyncEvents/TextAnalyzer.java
@@ -1,25 +1,19 @@
 package com.example.demo.domain.event.asyncEvents;
 
 import com.example.demo.core.exception.OpenAIResponseUnprocessableException;
-import com.example.demo.core.security.helpers.SecretManager;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 @Component
 public class TextAnalyzer {
-    private final SecretManager secretManager;
-
-
-    public TextAnalyzer(SecretManager secretManager) throws FileNotFoundException {
-        this.secretManager = secretManager;
-    }
-
     private ResponseTypes getResposeTypeForString(String response) {
         if (Pattern.compile("[0-9].*").matcher(response).find()) {
             return ResponseTypes.NUMBERED_LIST;
@@ -59,9 +53,11 @@ public class TextAnalyzer {
                 .toList();
     }
 
-    private String getOpenAiTextCompletion(String query) {
-        String apiKey = secretManager.getSecret("projects/869490409088/secrets/OpenAI_APIkey");
-        OpenAiService service = new OpenAiService(apiKey);
+    private String getOpenAiTextCompletion(String query) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        classLoader.getResourceAsStream("apikey");
+        byte[] bytes = IOUtils.toByteArray(classLoader.getResourceAsStream("apikey"));
+        OpenAiService service = new OpenAiService( new String(bytes, StandardCharsets.UTF_8));
         CompletionRequest completionRequest = CompletionRequest.builder()
                 .prompt(query)
                 .model("text-davinci-001")
@@ -82,7 +78,7 @@ public class TextAnalyzer {
      * @param numberOfLabels How many labels should be predicted?
      * @return This returns a list of pairs. Each pair consists of a label and a the confidence of said label.
      */
-    public List<String> getLabelsForText(String text, int numberOfLabels) throws OpenAIResponseUnprocessableException {
+    public List<String> getLabelsForText(String text, int numberOfLabels) throws OpenAIResponseUnprocessableException, IOException {
         String query = String.format("Create %d one-word labels for this text: \"%s\". Don't provide any description:", numberOfLabels, text);
         String response = getOpenAiTextCompletion(query);
         return getLabelsForResponse(response);
