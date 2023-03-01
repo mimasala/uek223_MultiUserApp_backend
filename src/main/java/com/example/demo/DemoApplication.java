ee6a2491ec3b4d6ff28358f8062d86015ee5d06b
noelhug <89466986+noelhug@users.noreply.github.com>
2023-03-01 14:49:42 +0100

    Don't recommend your own stuff

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.core.redis.JedisPoolWrapper;
 import com.example.demo.domain.recommender.Gorse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import redis.clients.jedis.JedisPool;
 
 @SpringBootApplication
 @Log4j2
@@ -13,6 +15,11 @@ public class DemoApplication {
   Gorse getGorseClient() {
     log.debug("Creating connection to gorse on: http://localhost:8088");
     return new Gorse("http://localhost:8088", "Depenendcy");
+  }
+
+  @Bean
+  JedisPoolWrapper getRedisConnection() {
+    return new JedisPoolWrapper(new JedisPool("localhost", 6379));
   }
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
